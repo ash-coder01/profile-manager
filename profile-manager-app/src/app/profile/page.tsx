74599912580e048483fc4410019843988aacdaf1
Ashlesha <ashleshadhanjal@gmail.com>
2023-08-20 20:37:55 +0530

    Set up profile page route

---
+++ profile-manager-app/src/app/profile/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function profileScreen() {
+  return (
+    <main className="min-h-screen min-w-max flex items-center justify-center">
+      My Profile Page
+    </main>
+  )
+}
